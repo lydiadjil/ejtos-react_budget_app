79b833d86a3f0b869330ce599ad246228805547f
lydiadjil <lydiaferiel2006@gmail.com>
2025-05-02 22:52:55 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-import React, { useContext, useState } from 'react';
-import { AppContext } from '../context/AppContext';
+import React from 'react';
+import Budget from './components/Budget';
 
-const Budget = () => {
-    const { budget } = useContext(AppContext);
-    const [newBudget, setNewBudget] = useState(budget);
-    const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
-    }
+const App = () => {
     return (
-<div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
-</div>
+        <div className='container'>
+            {/* Add Budget component here */}
+            <div className='col-sm'>
+                <Budget />
+            </div>
+        </div>
     );
 };
-export default Budget;// Budget component
-<div className='col-sm'>
-    <Budget />
-</div>
+
+export default App;
