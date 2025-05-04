5fe1cbf97cc3c035b6e548aecde4c3f690b9eca2
lydiadjil <lydiaferiel2006@gmail.com>
2025-05-04 09:26:54 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
+import ExpenseItem from './components/ExpenseItem';
+import AllocationForm from './components/AllocationForm';
+import { AppProvider } from './context/AppContext';
 
 const App = () => {
     return (
-        <div className='container'>
-            {/* Add Budget component here */}
-            <div className='col-sm'>
-                <Budget />
+        <AppProvider>
+            <div className='container'>
+                <h1 className='mt-3'>Company's Budget Allocation</h1>
+                <div className='row mt-3'>
+                    <div className='col-sm'>
+                        <Budget />
+                    </div>
+
+                    <div className='col-sm'>
+                        <Remaining />
+                    </div>
+
+                    <div className='col-sm'>
+                        <ExpenseTotal />
+                    </div>
+                </div>
+
+                <h3 className='mt-3'>Allocation</h3>
+                <div className='row '>
+                    <div className='col-sm'>
+                        <ExpenseList />
+                    </div>
+                </div>
+
+                {/* Note: Normally ExpenseItem is used INSIDE ExpenseList, not separately */}
+                {/* <div className='col-sm'>
+                    <ExpenseItem />
+                </div> */}
+
+                <h3 className='mt-3'>Change allocation</h3>
+                <div className='row mt-3'>
+                    <div className='col-sm'>
+                        <AllocationForm />
+                    </div>
+                </div>
             </div>
-        </div>
+        </AppProvider>
     );
 };
 
